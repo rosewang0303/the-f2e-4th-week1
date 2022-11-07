364c8a21da886258a3fcbe8d11b42e72318ae16e
rose <rosewang0303@gmail.com>
2022-11-07 10:53:59 +0800

    ADD: Loading rwd

---
+++ components/loading.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="loading">
+    <div class="loading__wrap">
+      <div>
+        <div class="loading__text">
+          <div>Loading...</div>
+        </div>
+        <img src="@/assets/img/tank_1.gif" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadIng',
+}
+</script>
+
+<style lang="scss">
+  .loading {
+    background-color: $black;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    z-index: 1;
+    @include flexCenter;
+    &__wrap {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 50%;
+    }
+    &__text {
+      font-size: 36px;
+      line-height: 42px;
+      color: $white;
+      margin-bottom: 31px;
+      @include flexCenter;
+      text-align: center;
+      div {
+        @include animateTyping();
+      }
+    }
+    img {
+      width: 164px;
+      height: 66px;
+      margin: 0 auto;
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="index">
     <!-- <span class="text" data-storke="index 測試">index 測試</span> -->
+    <Transition name="fade">
+      <Loading v-if="loading" />
+    </Transition>
     <Navbar />
   </div>
 </template>
@@ -8,6 +11,18 @@
 <script>
 export default {
   name: 'Index',
+
+  data() {
+    return {
+      loading: true,
+    }
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 2000);
+  },
 }
 </script>
 
